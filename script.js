b9e3e20b95502893cce0069815ec56fc50345762
strangka <strangka@student.21-school.ru>
2025-05-01 20:09:11 +0500

    all

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const buttons = document.querySelectorAll('.rate_button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach((btn) => btn.classList.remove('selected'));
+
+        button.classList.add('selected');
+    });
+});
+
 function showWindow(id) {
     const main = document.querySelector('.main');
     main.style.display = 'none';
@@ -5,3 +15,9 @@ function showWindow(id) {
     const result = document.getElementById(id);
     result.style.display = 'block';
 }
+
+function checkScore(id) {
+    const scoreText = document.getElementById('scoreText');
+    scoreText.textContent = 'You selected ' + id + ' out of 5';
+}
+
